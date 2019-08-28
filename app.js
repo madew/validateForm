a7df68e4aa38b564712908c1085311b023c3cf36
Michal <michal@MacBook-Pro-Michal.local>
2019-08-28 16:55:35 +0200

    Added password validation

--- app.js
+++ app.js
@@ -1,14 +1,29 @@
 const username = document.getElementById("username-input");
+const password = document.getElementById("password-input");
+const repeatPassword = document.getElementById("repeat-password-input");
 const usernameRequirements = document.querySelector(".username-requirements");
+const passwordRequirements = document.querySelector(".password-requirements");
+const repeatPasswordRequirements = document.querySelector(
+  ".repeat-password-requirements"
+);
 
 // Validators
 function validateUsername() {
-  // TODO Check for length checkForMinLength(username, 3)
-  // TODO Check for contains only characters and letters
   return (
     checkForLettersAndNumbers(username) &&
     checkForMinAndMaxLength(username, 3, 10)
   );
+}
+
+function validatePassword() {
+  return (
+    checkForMinAndMaxLength(password, 8, 100) &&
+    checkForCorrectPassword(password)
+  );
+}
+
+function validateRepeatedPassword() {
+  if (password.value === repeatPassword.value) return true;
 }
 
 // Utility
@@ -24,6 +39,12 @@ function checkForLettersAndNumbers(input) {
   return true;
 }
 
+function checkForCorrectPassword(input) {
+  const regEx = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/;
+  if (!input.value.match(regEx)) return false;
+  return true;
+}
+
 // Event Listeners
 username.addEventListener("keyup", () => {
   if (validateUsername()) {
@@ -32,5 +53,25 @@ username.addEventListener("keyup", () => {
   } else {
     usernameRequirements.firstElementChild.classList.add("invalid");
     usernameRequirements.firstElementChild.classList.remove("valid");
+  }
+});
+
+password.addEventListener("keyup", () => {
+  if (validatePassword()) {
+    passwordRequirements.firstElementChild.classList.add("valid");
+    passwordRequirements.firstElementChild.classList.remove("invalid");
+  } else {
+    passwordRequirements.firstElementChild.classList.add("invalid");
+    passwordRequirements.firstElementChild.classList.remove("valid");
+  }
+});
+
+repeatPassword.addEventListener("keyup", () => {
+  if (validateRepeatedPassword()) {
+    repeatPasswordRequirements.firstElementChild.classList.add("valid");
+    repeatPasswordRequirements.firstElementChild.classList.remove("invalid");
+  } else {
+    repeatPasswordRequirements.firstElementChild.classList.add("invalid");
+    repeatPasswordRequirements.firstElementChild.classList.remove("valid");
   }
 });
